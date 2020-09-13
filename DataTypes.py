ad3b26b00a3d33c5aee5f2899a765f405cbfd2c4
BreadBro <48104087+BreadBro@users.noreply.github.com>
2020-09-13 13:51:58 -0500

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,17 @@
+str='Hello'
+a=4
+b=2.0
+c=3
+d=True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(str))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print('A modulus is what we use to get the remainder of a division problem.')
