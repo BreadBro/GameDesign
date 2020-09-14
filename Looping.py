734bfa9d32d2bec90e029303e350210007e0fe70
SafinGH <48104087+SafinGH@users.noreply.github.com>
2020-09-14 15:59:27 -0500

    Add files via upload

---
+++ Looping.py
@@ -0,0 +1,23 @@
+"""
+print("1 2 3 4 5 6 7 8 9")
+print("1\n"+"2\n"+"3\n"+"4\n")
+print("1\n")
+print("2 2\n")
+print("3 3 3\n" )
+"""
+#For loop time
+for x in range(1, 10):
+    print(x)
+for x in range(1, 5):
+    print(x)
+for x in range(1, 2):
+    print(x)
+for x in range(2, 3):
+    print(x)
+    print(x)
+for x in range(3, 4):
+    print(x)
+    print(x)
+    print(x)
+#for x in range(10):
+    #print(x)
