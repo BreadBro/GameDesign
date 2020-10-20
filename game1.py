7cbb167696abbb4d467412d41215242fcab9ae9e
BreadBro <48104087+BreadBro@users.noreply.github.com>
2020-10-20 11:06:31 -0500

    Update game1.py

--- game1.py
+++ game1.py
@@ -4,29 +4,31 @@ import sys
 import os
 
 name = input("\nWhat is your name?\n")
-words = ['python','java','php','JavaScript','computer','geeks','keyboard','laptop','headphones','hardware','software']
+words = ['python','java','php','javascript','computer','geeks','keyboard','laptop','headphones','hardware','software','msi','nvidia','intel','logitech','amd','playstation','xbox','nintendo','gamecube','ryzen','cpu','unity particle system']
 answer = input("Do you want to guess a word?\n")
 while answer == "Yes" or answer == "yes" or answer == "y" or answer == "Y":
     print("Good Luck " + name + "!")
     word = random.choice(words)
     print("Guess my word.")
     guesses = ''
+    final = ''
     turns = 10
     while turns > 0:
         failed = 0
         for char in word:
             if char in guesses:
-                print(char)
+                print(char,end='')
+                final = final+char
             else:
-                print("_")
+                print("_",end='')
                 failed += 1
-            if failed == 0:
+            if word in final:
                 print("You have won with", + turns, "guesses left.")
                 print("The word is:", word)
                 answer == input("Do you want to play again?")
                 if answer == "Yes" or answer == "yes" or answer == "y" or answer == "Y":
                     os.execl(sys.executable, sys.executable, *sys.argv) # completely restart program
-        guess = input("Guess a character: ") # This line was screwing up stuff for some reason so I had to do the line above ^ and completely restart the program. (Let's just say its one of those old arcade machines where you need to input your name for a high score every time.)
+        guess = input("\nGuess a character: ") # This line was screwing up stuff for some reason so I had to do the line above ^ and completely restart the program. (Let's just say its one of those old arcade machines where you need to input your name for a high score every time.)
         guesses += guess
         if guess not in word:
             turns -= 1
@@ -35,7 +37,7 @@ while answer == "Yes" or answer == "yes" or answer == "y" or answer == "Y":
             if turns == 0:
                 print("You have run out of guesses. You have failed.")
                 print("GAME OVER, You Lose.")
-                answer == input("Do you want to play again?")
+                answer == input("\nDo you want to play again?")
                 if answer == "Yes" or answer == "yes" or answer == "y" or answer == "Y":
                     break
 
